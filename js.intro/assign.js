e2b9d55907291251b98e27c254151431d7d120cb
sahadullah05 <sahad.rajabali75@gmail.com>
2026-02-20 07:08:33 -0800

    js.assigment+intro

---
+++ js.intro/assign.js
@@ -0,0 +1,160 @@
+            //  chapter#
+// Quetion: 1      
+//   alert("Hello World");  
+
+//  Quetion: 2     
+//   alert("Error! \n Please enter a valid password."); 
+
+//  Quetion: 3  
+//  alert("Welcome to JS world... \n   Coding Experince!");   
+
+//  Quetion: 4
+// alert("Welcome to JS world...");
+// alert("Coding Experince!");
+
+//  Quetion: 5
+// alert("Hello... I can run JS through my web browser's console");
+
+// quetion: 6
+//  console.log("Hello... I can run JS through my web browser's console");
+
+       
+                    // chapter#2
+  
+//  quetion: 1
+//   var username = "sahad"   
+  
+// quetion: 2
+// var myName = "Sahad Ullah"
+
+// quetion: 3
+// var message = "Hello World"
+
+// quetion: 4
+// alert(message)
+
+// quetion: 5
+// var name = "sahad"
+
+// var age = 20
+// var course = "Modern web and app development"
+// alert(name)
+// alert(age)
+// alert(course)
+
+// quetion: 6
+// var name = "sahad"
+// var age = 20
+// alert("I'm " + name + "\nI am " + age + " years old")
+
+// quetion: 7
+//  var name = "PIZZA"
+//  alert(name + "\n" + name.slice(0,4) + "\n" + name.slice(0,3) + "\n" + name.slice(0,2) + "\n" + name.slice(0,1))
+
+// quetion: 8
+// var email = "sahadullah123@gmail.com"
+// alert("My email address is " + email)
+
+
+
+// quetion: 9
+// document.write("Yah! I can write HTML content through JavaScript")
+
+
+                             // chapter#3
+
+// quetion: 1
+// var age= 20
+// alert ("I am " + age + " years old")
+
+// quetion: 2
+// alert("You have visited this site " + age + " times")
+
+// quetion: 3
+// var birthYear = 2005
+// document.write("my birth year is " + birthYear + "<br>" + "Data type of my declared variable is number")
+
+// quetion: 4
+//  var name = "John Doe"
+//  var product = "T-shirt"
+// var quantity = 5
+//  document.write(name + " ordered " + quantity + " " + product + "(s) on XYZ Clothing store")
+
+
+                //   chapter#4
+// quetion: 1
+// var name, age, course
+
+// quetion: 2
+// legal variable
+// var name
+// var age
+// var course
+
+// illegal variable
+// var 1name
+// var my name
+// var alert
+// let 1number  = 1
+// let @special  = 1
+// let -number = 1
+// let delete = "delete"
+// let animal name = ""
+// let kebab-case = ""
+
+// quetion: 3
+// console.log("<h1>Rules for naming JS variables</h1>" + "<br>" + "<br>" + "Variable names can onlycontain letters, numbers, $ and _. For example: $my_1stVariable" + "<br>" + "Variables must begin with a letter, $ or _. For example: $name, _name or name" + "<br>" + "Variable names are case sensitive"
+//       + "<br>" + "Variable names should not be JS keywords")
+
+                //   chapter# 5
+
+// quetion: 1
+// var num1 = 2
+// var num2 = 3
+// var sum = num1 + num2
+// console.log("The sum of " + num1 + " and " + num2 + " is " + sum)
+
+// quetion: 2
+// var num1 = 5
+// var num2 = 3
+// var difference = num1 - num2
+// console.log("The difference of " + num1 + " and " + num2 + " is " + difference)
+
+// var num1 = 4
+// var num2 = 6
+// var product = num1 * num2
+// console.log("The product of " + num1 + " and " + num2 + " is " + product)
+
+// var num1 = 10
+// var num2 = 2
+// var quotient = num1 / num2
+// console.log("The quotient of " + num1 + " and " + num2 + " is " + quotient)
+
+// quetion: 3
+// var num
+// console.log("Value after variable declaration is: " + num)
+// num = 5
+// console.log("Initial value: " + num)
+// num++
+// console.log("Value after increment is: " + num)
+// num = num + 7
+// console.log("Value after addition is: " + num)
+// num--
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js.intro/script.js
@@ -0,0 +1,32 @@
+// console.log(" name is sahad");
+
+
+// let username = undefined
+// console.log("name")
+
+
+// Data Types 
+
+// number 12
+// Boolean  true  false
+// undefined
+// string "value" 'string'  `str` => backtick
+
+
+
+// var , let , const 
+//  var name = "sahad"
+//  console.log(name)
+
+
+//  let num1  = 12;
+
+//  let num2 = 12
+
+//  console.log(num1 + " " + num2)
+
+//   let num1  = 12;
+
+//  let num2 = 12
+ 
+//  console.log(num1 + num2)
