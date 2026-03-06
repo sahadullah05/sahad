e70ab4646103ed474865cf947749875926f7f80a
sahadullah05 <sahad.rajabali75@gmail.com>
2026-03-06 03:27:35 -0800

    java.script assignment

---
+++ java.assignment/assignment.js
@@ -0,0 +1,235 @@
+//    chap 1
+// quetion 1
+// alert("hello!")
+
+// quetion 2
+// alert("javascript!alert \n \n welcome to the site") [\n used for line break]
+
+// quetion 3
+//  alert(" javascript Alert \n \n welcome to js land.... \n coding world")
+
+
+
+//  Quetion: 5
+//  alert("Hello... I can run JS through my web browser's console");
+
+
+   //chap2
+// Quetion: 1
+// var userName = "sahad"
+
+// Quetion: 2
+// var myName = "Sahad Ullah"
+
+// Quetion: 3
+// var message = "Hello world"
+// alert(message)
+
+// Quetion: 4
+//  var fullName = "sahad ullah"
+//  alert( fullName)
+// var age = 20 + " years old"
+// alert(age)
+// var profession = "Web & app development"
+// alert(profession )
+
+// var value = "pizza"
+// alert( value + "\n"+ value + "\n" + value + "\n")
+
+// var value = "pizz"
+// alert( value)
+// var value = "piz"
+// alert( value)
+
+// quetion 6
+// var email = "sahad@gmail.com"
+// alert(email)
+
+           // chap 3
+
+// quetion 1
+// var age = 20
+// alert("I am " +age + " years old" )
+
+// quetion 2
+// var visit = "You have visited this sites 15 times"
+// alert(visit)
+  
+// quetion 3
+// var birthYear = 2005
+// alert("my birth year is " + birthYear + "\n" + "Data type of my declared variable is number")
+
+
+// quetion 4
+// var name = "Adnan" 
+// var product = "watch"
+// var quantity = 4
+// console.log(name + " order " + quantity + " of " + product)
+
+      //  chap 4
+// quetion 1
+// var name, age, num
+
+// quetion 2
+// legal variable
+// var name
+// var age
+// var course
+
+// illegal variable
+// var 1name
+// var my name
+// var alert
+// let 1number  = 1
+// let @special  = 1
+// let -number = 1
+
+//  quetion 3
+// console.log( "<h1>Rules for naming JS variables</h1>" + "\n " + "names only contain num , letter, $" + "for example_variables" + "Variable names can onlycontain letters, numbers, $ , for example $name,name"
+//     + "\n" + "Variable names should not be JS keywords"
+//  )
+
+//     chap 5
+
+// quetion 1 
+//  var num1 = 5
+//  var num2 = 3
+// var num = num1 + num2
+// console.log("sum of " + num1 + " and " + num2 +" is " + num)
+
+// quetion 2
+//  var num1 = 5
+//  var num2 = 3
+// var num = num1 - num2
+// console.log("sum of " + num1 + " and " + num2 +" is " + num)
+
+//  var num1 = 5
+//  var num2 = 3
+// var num = num1 * num2
+// console.log("sum of " + num1 + " and " + num2 +" is " + num)
+
+//  var num1 = 5
+//  var num2 = 3
+// var num = num1 / num2
+// console.log("sum of " + num1 + " and " + num2 +" is " + num)
+
+// quetion 3
+// var value = 10
+// console.log("Value after variable declaration is " + value)
+
+// num = 5
+// console.log( value + num)
+
+// num++
+// console.log( value + num)
+
+// num = num + 7
+// console.log( value + num)
+
+// num--
+// console.log( value + num)
+
+// var reminder = num % 3
+// console.log( reminder)
+
+// quetion 4
+// var ticketPrice = 600
+// var cost = 5
+// console.log( "Total cost to buy " + cost + " tickets to a movie is " + ticketPrice)
+
+//  quetion 5
+// var num = 5
+// var value = num * 1
+// console.log(num + " x " + 1 + " = " + num * 1)
+// console.log(num + " x " + 2 + " = " + num * 2)
+// console.log(num + " x " + 3 + " = " + num * 3)
+// console.log(num + " x " + 4 + " = " + num * 4)
+// console.log(num + " x " + 5 + " = " + num * 5)
+
+
+// quetion: 7
+// var item1Price = 650
+// var item2Price = 100
+// var item1Quantity = 3
+// var item2Quantity = 7
+// var shippingCharges = 100
+// var totalCost = (item1Price * item1Quantity) + (item2Price * item2Quantity) + shippingCharges
+// console.log("Price of item 1 is " + item1Price)
+// console.log("Quantity of item 1 is " + item1Quantity)
+// console.log("Price of item 2 is " + item2Price)
+// console.log("Quantity of item 2 is " + item2Quantity)
+// console.log("Shipping Charges " + shippingCharges)
+// console.log("Total cost of your order is " + totalCost)
+
+//      chap 6 to 9
+// quetion 1
+// var a = 10
+// console.log( "Result" + "\n " + "the value is "+ a )
+ 
+// a = ++a
+// console.log( "the value of ++a is " + a )
+// console.log("now the value is :" + a)
+
+// a = a++
+// console.log( "the value of ++a is " + a )
+// console.log("now the value is :" + a)
+
+// a = --a
+// console.log( "the value of ++a is " + a )
+// console.log("now the value is :" + a)
+
+// a = a--
+// console.log( "the value of ++a is " + a )
+// console.log("now the value is :" + a)
+
+// quetion 2
+//  var a = 2
+//  var b =1
+//  var result = --a - --b + ++b + b--
+            //    1  - 0 + 1 + 1
+// console.log ("a is "+ result) 
+// console.log("b is " + b)
+// console.log("result is " + result)
+
+// quetion3
+//   var Name = prompt ("Enter your name")
+//   var greet = "Hello!"
+//   console.log(greet + Name )
+
+// quetion 5 "not understand this one"
+// var num =prompt("Enter a number")
+//  num=5
+// if(num){
+//      console.loge( "The multiplication table of " + num + ":")
+// }else {
+//       console.loge( "The multiplication table of " + num + ":")
+// }
+//  for (let i = 1; i <= 10; i++) {
+//     console.log(num + " x " + i + " = " + (num * i)) }
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
