aad7a89b04d856b64bfc9dc11737094ed39dc6ab
sahadullah05 <sahad.rajabali75@gmail.com>
2026-03-09 13:23:48 -0700

    chap 1 to 16

--- java.assignment/assignment.js
+++ java.assignment/assignment.js
@@ -207,6 +207,241 @@
 //  for (let i = 1; i <= 10; i++) {
 //     console.log(num + " x " + i + " = " + (num * i)) }
 
+// chap 9 to 11
+
+// quetion 1
+// city = "karachi"
+// var city = prompt ('Enter your city')
+// console.log("wellcome to the city of light " + city)
+
+// quetion 2
+
+// var gender = prompt( "Enter your gender", "Male")
+// if( gender === "Male"){
+//       console.log( "Good morning sir!")
+// }if (gender === "female"){
+//       console.log ( "Good morning ma'am")
+// }
+
+// quetion 3
+// var traffic = prompt("traffi signal is", "RED", "yello", "green"// )
+// if (traffic === "RED"){
+//       console.log("you have to stop")
+// }
+// var traffic = prompt("traffi signal is", "yellow")
+//  if (traffic === "yellow"){
+//       console.log("Be ready")
+// }
+// var traffic = prompt("traffi signal is", "green")
+// if (traffic === "green"){
+//       console.log("now you can go")
+// } 
+
+// quetion 4
+// var fuel = prompt("Enter your remaining fuel in car", "0.25")
+// if (fuel < 0.25){
+//       console.log("Please refill the fuel in your car")
+// }
+
+// quetion 5
+// var a = 4
+// if (++a === 5){
+//       console.log("given condition for variable a is true")
+// }
+// var b = 82
+// if (b++ === 83){
+//       console.log("given condition for variable b is true")
+// }
+// var c = 12
+// if (c++ === 13){
+//       console.log("condition 1 is true")
+// }
+// if (c === 13){
+//       console.log("condition 2 is true")
+// }
+// var materialCost = 20000
+// var laborCost = 2000
+// var totalCost = materialCost + laborCost
+// if (totalCost === laborCost + materialCost){
+//       console.log("The cost equals")
+// }
+// if (true){
+//       console.log("True")
+// }
+// if (false){
+//       console.log("False")
+// }
+// if ("car" < "cat"){
+//       console.log("car is smaller than cat")
+// }
+
+// quetion 6
+// var sub1 = prompt("Enter marks obtained in subject 1")
+// var sub2 = prompt("Enter marks obtained in subject 2")
+// var sub3 = prompt("Enter marks obtained in subject 3")
+// var totalMarks = 300
+// var obtainedMarks = parseInt(sub1) + parseInt(sub2) + parseInt(sub3)
+// var percentage = (obtainedMarks / totalMarks) * 100
+// console.log("Total Marks: " + totalMarks)
+// console.log("Marks Obtained: " + obtainedMarks)
+// console.log("Percentage: " + percentage + "%")
+
+// quetion 7
+// var secretNum = 7
+// var guess = prompt("Guess the secret number between 1 and 10")
+// if (guess == secretNum){
+//       console.log("Bingo! Correct answer")
+// } else if (guess == secretNum + 1 || guess == secretNum - 1){
+//       console.log("Close enough to the correct answer")
+// } else {
+//       console.log("Wrong guess! Try again.")
+// }
+
+// quetion 8
+// var num = prompt("Enter a number")
+// if (num % 3 === 0){
+//       console.log(num + " is divisible by 3")
+// } else {
+//       console.log(num + " is not divisible by 3")
+// }
+
+// quetion 9
+// var num = prompt("Enter a number")
+// if (num % 2 === 0){
+//       console.log(num + " is an even number")
+// } else {
+//       console.log(num + " is an odd number")
+// } 
+
+// chap 12 to 13
+
+// quetion 1
+// var char = prompt("Enter a character")
+// if (char >= 'A' && char <= 'Z') {
+//       console.log(char + " is an uppercase letter")
+// } else if (char >= 'a' && char <= 'z') {
+//       console.log(char + " is a lowercase letter")
+// } else {
+//       console.log(char + " is not an alphabetic character")
+// }
+
+// quetion 2
+// var num1 = prompt("Enter first integer")
+// var num2 = prompt("Enter second integer")
+// if (num1 > num2){
+//       console.log(num1 + " is larger than " + num2)
+// } else if (num2 > num1){
+//       console.log(num2 + " is larger than " + num1)
+// } else {
+//       console.log("Both numbers are equal")
+// }
+
+// quetion 3
+// var num = prompt("Enter a number")
+// if (num > 0){
+//       console.log(num + " is a positive number")
+// } else if (num < 0){
+//       console.log(num + " is a negative number")
+// } else {
+//       console.log("The number is zero")
+// }
+
+// quetion 4
+// var char = prompt("Enter a character")
+// if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ||
+//       char === 'A' || char === 'E' || char === 'I' || char === 'O' || char === 'U') {
+//       console.log(char + " is a vowel")
+// } else {
+//       console.log(char + " is not a vowel")
+// }
+
+// quetion 5
+// var password = "myPassword123"
+// var userInput = prompt("Enter your password")
+// if (userInput === ""){
+//       console.log("Please enter your password")
+// } else if (userInput === password){
+//       console.log("Correct! The password you entered matches the original password")
+// } else {
+//       console.log("Incorrect password")
+// }
+
+//      chap 14 to 16
+// quetion 1
+// var studentNames = []
+
+// quetion 3
+// var stringArray = ["apple", "banana", "cherry"]
+//     console.log(stringArray[2])
+
+// quetion 4
+// var numArray = [1, 2, 3, 4, 5]
+//     console.log(numArray[4])
+
+// quetion 5
+// var boolArray = [true, false, true, false]
+//     console.log(boolArray[0])
+
+// quetion 6
+// var mixedArray = ["hello", 42, true, "world", 3.14]
+//     console.log(mixedArray[3])
+
+// quetion 7
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"]
+// console.log("Qualifications:")
+
+// quetion 8
+// var students = ["Michael", "John", "Tony"]
+// var scores = [320, 230, 480]
+// console.log("Score of " + students[1] + " is " + scores[1] + ". Percentage: " + (scores[0] / 500 * 100) + "%")
+
+// quetion 9
+// var colorNames = ["red", "green", "blue"]
+// var newColor = prompt("Enter a color to add to the beginning of the array")
+// colorNames.unshift(newColor)
+// console.log(colorNames)
+
+// quetion 10
+// var colorNames = ["red", "green", "blue"]
+// var newColor = prompt("Enter a color to add to the end of the array")
+// colorNames.push(newColor)
+// console.log(colorNames)
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
