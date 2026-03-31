500fdae605c5f97e1615b304ef4376b2a900ec62
sahadullah05 <sahad.rajabali75@gmail.com>
2026-03-31 09:59:13 -0700

    21 to 30

---
+++ js assign 21 to 30/app.js
@@ -0,0 +1,149 @@
+//    chap 20 to 25
+// quetion1
+// let firstName = prompt("Enter your firstname")
+// let lastName =prompt("Enter your lastname") 
+
+// let fullName = firstName + " " + lastName
+// console.log ( "Hello!")
+// console.log(fullName)
+
+// quetion2
+
+// const mobile = "sumsung"
+// console.log ( "My phone is: sumsung galaxy s22 ultra plus "+ "\n" + " length of string " + mobile.length)
+
+// quetion3
+// const word = "pakistani"
+// let index = word.indexOf ("n") 
+// console.log("pakistani" + "\n" + "the index of 'n' " + index )
+
+// quetion4
+
+// const word = "Hello World!"
+// let index = word.lastIndexOf("l") 
+// console.log("string: Hello world!" + "\n" + "the index of 'l': " + index )
+
+// quetion5
+// const word = "pakistani"
+// let character = word.charAt("3") 
+// console.log("pakistani" + "\n" + "character at index  3: " + character )
+
+// quetion6
+// let firstName = prompt("Enter your firstname")
+// let lastName =prompt("Enter your lastname") 
+
+// let fullName = firstName.concat (' ' + lastName)
+// console.log ( "Hello!" + "\n"  + fullName + "\n" + " welcome!")
+
+//  quetion7
+// let word = "Hyderabad"
+// let newword = word.replace("Hyder" , "islam")
+// console.log("Hyderabad" + "\n" + "AFter replacing " + newword )
+
+// quetion8
+//  let text = "Ali and Sami are best friends and they play cricket and football together.";
+
+//  let message = text.replaceAll('and','&')
+//  console.log(text)
+//  console.log(message)
+ 
+// quetion 9
+
+// let str = "472"
+// let num = Number(str)
+// console.log("Value: " + str   +"| Type:" + typeof str + "\n" +
+//     "Value:" + num +"| Type:" + typeof num
+// )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    chap 26 to 30
+    // quetioin1
+    //  let num = 12.5134
+   // let roundNum = Math.round(12.6)
+    // console.log(roundNum)
+    // let ceilNum = Math.ceil(12.12)
+    // console.log(ceilNum)
+    // let floorNum = Math.floor(12.12)
+    // console.log(floorNum)
+
+// quetioin2
+//      let num = -2.673
+//    let roundNum = Math.round(-2.673)
+//     console.log(roundNum)
+//     let ceilNum = Math.ceil(-2.673)
+//     console.log(ceilNum)
+//     let floorNum = Math.floor(-2.673)
+//     console.log(floorNum)
+
+// quetion3
+// let absoluteNum = Math.abs(-4)
+// console.log("The absolute value is : " +  absoluteNum)
+
+
+
+// // quetion4
+// let randomNum = Math.random()
+// console.log("The random dice value : "+Math.round(Math.random()* 6))
+
+// quetion5
+
+// let randomNum = prompt( Math.random() < 1 ? 0 : 2)
+// let toss = randomNum === 0 ? "head" : "tail"
+// console.log(toss)
+
+// quetion 6
+// let randomNum = Math.random(84)
+// console.log("random number b/w 1 to 100 : "+Math.round(Math.random() * 100))
+
+// quetion 7
+// let randomNum = prompt ("Enter your weight: " )
+// let weight = parseFloat(randomNum)
+// console.log("The weight of user is: " + weight + "kg")
+
+// quetion8
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// var userNumber = prompt("Guess the secret number (between 1 and 10):");
+
+// userNumber = parseInt(userNumber);
+// if (userNumber === secretNumber) {
+//     alert(" Congratulations! You guessed the correct number!");
+// } else {
+//     alert("Sorry! The secret number was " + secretNumber);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
