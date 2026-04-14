1add0f775dcf33afc4012ebae92089d86a85807c
sahadullah05 <sahad.rajabali75@gmail.com>
2026-04-14 04:43:22 -0700

    logics

---
+++ 4 logics/app.js
@@ -0,0 +1,57 @@
+//     "maximum"
+
+// let num = [10, 20, 5, 80]
+
+// let maxNum = num[0]
+
+// for (let i = 1; i < num.length; i++) {
+   
+//     if (num[i] > maxNum) {
+//         maxNum = num[i]
+//     }
+// }
+// console.log("maximum num:" + maxNum)
+
+
+            //  "minimum"
+
+// let num = [10, 20, 5, 80]
+
+// let minNum = num[0]
+
+// for (let i = 1; i < num.length; i++) {
+   
+//     if (num[i] < minNum) {
+//         minNum = num[i]
+//     }
+// }
+// console.log("minimum number:" + minNum)
+
+            // password generate
+//  var randomPassword = "joi&adsuo%fd$dtr@pak"
+//  var length = 8
+//  var password = ""
+
+//  for ( var i = 1; i < length ; i++){
+//     var randomIndx = Math.floor( Math.random() * randomPassword.length)
+//     password = randomPassword [randomIndx] 
+
+//  }
+//  console.log("Password: " + password )
+
+    //  ramzan time line
+
+//     let Ramadan2027 = new Date("February 7, 2027")
+// let currentDate = new Date()
+
+
+// let remainingTime = Ramadan2027.getTime() - currentDate.getTime()
+
+
+// let diff = 1000 * 60 * 60 * 24 * 30 * 12
+
+// console.log(Math.round(remainingTime / diff))
+ 
+
+
+
